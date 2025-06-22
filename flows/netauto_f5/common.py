2d51f2597b276af946a000e58103f9a3499f1e89
Lanca Matej <matej.lanca@alef.com>
2025-06-22 14:58:50 +0200

    add validate task

---
+++ flows/netauto_f5/common.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from prefect import task, get_run_logger
+
+class WebhookData(BaseModel):
+    node_id: str
+    checksum: str
+    target_id: str
+    storage_id: str
+    target_kind: str
+    checksum_previous: str
+    storage_id_previous: str
+    artifact_definition_id: str
+
+class WebhookPayload(BaseModel):
+    data: WebhookData
+    id: str
+    branch: str
+    account_id: str
+    occured_at: str
+    event: str
+
+@task()
+def validate_webhook_data(webhook_data: dict) -> WebhookPayload:
+    """
+    Validates the incoming webhook data against the WebhookPayload model.
+    Raises a ValueError if validation fails.
+    """
+    logger = get_run_logger()
+    logger.info("Validating webhook data...")
+    try:
+        return WebhookPayload(**webhook_data)
+    except Exception as e:
+        raise ValueError(f"Invalid webhook data: {e}") from e
--- flows/netauto_f5/flex.py
+++ flows/netauto_f5/flex.py
@@ -1,7 +1,9 @@
 from prefect import flow
-import requests
+from common import validate_webhook_data
+from typing import Dict
 
 @flow(log_prints=True)
-def process_flex_application():
+def process_flex_application(webhook_data: Dict):
     print("Processing Netauto Flex Application...")
-    print("updated flow")
+    webhook_data = validate_webhook_data(webhook_data)
+    
